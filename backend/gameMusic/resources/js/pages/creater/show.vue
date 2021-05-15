19ab4708e885247f353cbe7fde94e02a4b6a28a1
kawada <vgnsl94@gmail.com>
2021-05-16 00:40:54 +0900

    プロフィールページ少し作成

--- backend/gameMusic/resources/js/pages/creater/show.vue
+++ backend/gameMusic/resources/js/pages/creater/show.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>
-    クリエイター詳細ページ
-  </div>
+  <div class="container">
+      <div class="row my-3">
+
+        <!-- 左側 -->
+        <div class="col-sm-3 col-xs-12">
+          <div class="creater_image d-flex justify-content-start mb-3">
+            <img src="/images/498467_s.jpg" class="rounded-circle">
+          </div>
+
+          <h3>Creater Name</h3>
+          <a class="btn btn-outline-primary mt-2">このアーティストをフォロー</a>
+        </div>
+
+
+
+        <!-- 右側 -->
+        <div class="col-sm-9 col-xs-12 right">
+          <div class="profile_title">
+            <h2>Creater Nameさんのプロフィール</h2>
+          </div>
+        </div>
+
+      </div>
+    </div>
 </template>
 
 <script>
@@ -10,6 +31,13 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.creater_image img {
+  height: 144px;
+}
+
+.profile_title h2 {
+  font-weight: bold;
+}
 
 </style>
