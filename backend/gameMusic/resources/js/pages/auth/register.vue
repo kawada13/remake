e006515e0ce76af2adadbadb86d9f5fec99e8980
kawada <vgnsl94@gmail.com>
2021-05-18 00:37:34 +0900

    微修正

--- backend/gameMusic/resources/js/pages/auth/register.vue
+++ backend/gameMusic/resources/js/pages/auth/register.vue
@@ -2,47 +2,46 @@
   <div class="new-audio">
 
     <div class="container">
+      
       <div class="content-top-title">
         <h1>新規登録</h1>
       </div>
 
-    <div class="form_register d-flex justify-content-center my-5">
-      <div class="card" style="width: 24rem;">
-        <div class="card-body">
-          <form @submit.prevent="register">
-            <div class="form-group">
-              <div class="d-flex justify-content-start"><label for="name">お名前</label></div>
-              <input type="text" class="form-control form-control-lg" id="name" v-model="name">
-              <div class="d-flex justify-content-start"><small class="form-text text-muted">登録後変更可能です。</small></div>
-              <div class="alert alert-danger mt-2" role="alert" v-if="errors.name.required">
-                お名前の入力は必須です！
+      <div class="form_register d-flex justify-content-center my-5">
+        <div class="card" style="width: 24rem;">
+          <div class="card-body">
+            <form @submit.prevent="register">
+              <div class="form-group">
+                <div class="d-flex justify-content-start"><label for="name">お名前</label></div>
+                <input type="text" class="form-control form-control-lg" id="name" v-model="name">
+                <div class="d-flex justify-content-start"><small class="form-text text-muted">登録後変更可能です。</small></div>
+                <div class="alert alert-danger mt-2" role="alert" v-if="errors.name.required">
+                  お名前の入力は必須です！
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <div class="d-flex justify-content-start"><label for="email">メールアドレス</label></div>
-              <input type="email" class="form-control form-control-lg" id="email" v-model="email">
-              <div class="alert alert-danger mt-2" role="alert" v-if="errors.email.required">
-                メールアドレスの入力は必須です！
+              <div class="form-group">
+                <div class="d-flex justify-content-start"><label for="email">メールアドレス</label></div>
+                <input type="email" class="form-control form-control-lg" id="email" v-model="email">
+                <div class="alert alert-danger mt-2" role="alert" v-if="errors.email.required">
+                  メールアドレスの入力は必須です！
+                </div>
               </div>
-            </div>
-            <div class="form-group">
-              <div class="d-flex justify-content-start"><label for="password">パスワード</label></div>
-              <input type="password" class="form-control form-control-lg" id="passsword" v-model="password">
-              <div class="d-flex justify-content-start"><small class="form-text text-muted">6文字以上で入力してください。</small></div>
-              <div class="alert alert-danger mt-2" role="alert" v-if="errors.password.required">
-                パスワードの入力は必須です！
+              <div class="form-group">
+                <div class="d-flex justify-content-start"><label for="password">パスワード</label></div>
+                <input type="password" class="form-control form-control-lg" id="passsword" v-model="password">
+                <div class="d-flex justify-content-start"><small class="form-text text-muted">6文字以上で入力してください。</small></div>
+                <div class="alert alert-danger mt-2" role="alert" v-if="errors.password.required">
+                  パスワードの入力は必須です！
+                </div>
+                <div class="alert alert-danger mt-2" role="alert" v-if="errors.password.size">
+                  6文字以上を指定してください！
+                </div>
               </div>
-              <div class="alert alert-danger mt-2" role="alert" v-if="errors.password.size">
-                6文字以上を指定してください！
-              </div>
-            </div>
-            <button type="submit" class="btn btn-primary my-4">登録する</button>
-          </form>
+              <button type="submit" class="btn btn-primary my-4">登録する</button>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-      
-
 
     </div>
   </div>
