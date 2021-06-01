5a9a5aac192ccef4d63ab9b9740b5f30474d7eed
kawada <vgnsl94@gmail.com>
2021-06-01 17:31:26 +0900

    follow favoriteテーブル作成

---
+++ backend/gameMusic/database/migrations/2021_06_01_171000_create_favorites_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favorites', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unsigned()->index();
+            $table->unsignedBigInteger('audio_id')->unsigned()->index();
+            $table->timestamps();
+
+            // 外部キー設定
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('audio_id')->references('id')->on('audios')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favorites');
+    }
+}
---
+++ backend/gameMusic/database/migrations/2021_06_01_171018_create_user_follow_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUserFollowTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_follow', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('follow_id');
+            $table->unsignedBigInteger('follower_id');
+            $table->timestamps();
+
+            // 外部キー設定
+            $table->foreign('follow_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('follower_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_follow');
+    }
+}
