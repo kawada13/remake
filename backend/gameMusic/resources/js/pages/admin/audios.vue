21b5ffbd8cfe72ae1f4335ec942bb049a43ad880
kawada <vgnsl94@gmail.com>
2021-06-11 14:10:04 +0900

    オーディオ一覧を作成

--- backend/gameMusic/resources/js/pages/admin/audios.vue
+++ backend/gameMusic/resources/js/pages/admin/audios.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-    audios
+  <div class="container">
+
+     <!-- ローディング中 -->
+    <div class="my-5" v-if="loading">
+      <Loader />
+    </div>
+
+
+    <div class="card mt-2" v-if="!loading">
+        <h3 class="card-header">
+          登録オーディオ
+        </h3>
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">タイトル</th>
+              <th scope="col">作成者</th>
+              <th scope="col">登録日時</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(audio, i) in audios" :key="i">
+              <th scope="row">{{i + 1}}</th>
+              <td><a href @click="$router.push({ name: 'usershow', params: { id: `${audio.id}` }})">{{ audio.title }}</a></td>
+              <td><a href @click="$router.push({ name: 'usershow', params: { id: `${audio.user_id}` }})">{{ audio.user.name }}</a></td>
+              <td>{{audio.created_at | fromiso}}</td>
+            </tr>
+          </tbody>
+        </table>
+    </div>
+
   </div>
 </template>
 
