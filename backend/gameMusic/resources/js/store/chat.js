0171c8ac4c766224aa6eb97ac209cc9ad057e124
kawada <vgnsl94@gmail.com>
2021-06-14 17:19:10 +0900

    storeにchat関連登録

--- backend/gameMusic/resources/js/store/chat.js
+++ backend/gameMusic/resources/js/store/chat.js
@@ -1,11 +1,16 @@
 const state = {
+  chatRooms: [],
+  chatMessages: [],
 }
 
 const getters = {}
 
 const mutations = {
-  setDupError(state, judge) {
-    state.duplication = judge
+  setChatRooms(state, data) {
+    state.chatRooms = data.chat_rooms
+  },
+  setChatMessages(state, data) {
+    state.chatMessages = data.chat_messages
   },
 
 }
@@ -16,6 +21,28 @@ const actions = {
     await axios.post(`/api/message/${id}`, data)
     .then(res => {
       console.log(res.data);
+    })
+    .catch(e => {
+      console.log(e.response);
+    })
+  },
+  // ログインユーザーのチャットルーム一覧
+  async getChatRooms({ commit }) {
+    await axios.get(`/api/chatRooms`)
+    .then(res => {
+      console.log(res.data);
+      commit('setChatRooms', res.data)
+    })
+    .catch(e => {
+      console.log(e.response);
+    })
+  },
+  // ある相手とのチャット履歴取得
+  async getChatMessages({ commit }, id) {
+    await axios.get(`/api/message/${id}`)
+    .then(res => {
+      console.log(res.data);
+      commit('setChatMessages', res.data)
     })
     .catch(e => {
       console.log(e.response);
